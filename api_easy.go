3d9be9cafa0a9d07b4adc58faecdfdb035691b7d
Wandergluf <104331639+Wandergluf@users.noreply.github.com>
2021-11-30 21:24:30 +0000

    sched: Add controls for sched features

--- api_easy.go
+++ api_easy.go
@@ -2,4 +2,5 @@ package fmr
 
 // Parse returns parse trees for rule <start> at beginning
 func (g *Grammar) Parse(text string, starts ...string) ([]*Node, error) {
-	return g.e
+	return g.extract(func(text string, starts ...string) ([]*Parse, error) {
+		p
