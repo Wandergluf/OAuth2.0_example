49c972918a62b6a8589e6484d59ad117446581d8
Wandergluf <104331639+Wandergluf@users.noreply.github.com>
2021-12-08 13:49:14 +0000

    Add compiled css

--- api_easy.go
+++ api_easy.go
@@ -4,4 +4,8 @@ package fmr
 func (g *Grammar) Parse(text string, starts ...string) ([]*Node, error) {
 	return g.extract(func(text string, starts ...string) ([]*Parse, error) {
 		p, err := g.EarleyParse(text, starts...)
-		if err != nil 
+		if err != nil {
+			return nil, err
+		}
+		return []*Parse{p}, nil
+	}, text, s
