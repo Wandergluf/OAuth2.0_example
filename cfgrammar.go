94aa59b23883b72c5112befdc62e13c46efe3867
Wandergluf <104331639+Wandergluf@users.noreply.github.com>
2022-01-22 09:40:18 +0000

    Fixed spaces

--- cfgrammar.go
+++ cfgrammar.go
@@ -79,3 +79,60 @@ func grammarFromString(d, name, dir string, files map[string]int) (*Grammar, err
 	if err != nil {
 		return nil, err
 	}
+	files[name]++
+	if Debug {
+		fmt.Println("loaded ", name, files)
+	}
+	return g, nil
+}
+
+func (p *parser) posInfo() string {
+	return fmt.Sprintf("%s%s", p.fname, p.current)
+}
+
+func (p *parser) next() rune {
+	if p.pos >= len(p.input) {
+		p.width = 0
+		return eof
+	}
+	r, w := utf8.DecodeRuneInString(p.input[p.pos:])
+	if r == utf8.RuneError {
+		return eof
+	}
+	p.width = w
+	p.pos += w
+	if p.info[p.pos] == nil {
+		if p.current == nil {
+			p.current = &position{1, w, string(r)}
+		} else {
+			if r == '\n' {
+				p.current = &position{p.current.row + 1, w, string(r)}
+			} else {
+				p.current = &position{p.current.row, p.current.col + w, string(r)}
+			}
+		}
+		p.info[p.pos] = p.current
+	} else {
+		p.current = p.info[p.pos]
+	}
+	return r
+}
+
+func (p *parser) eat(expected rune) error {
+	if r := p.next(); r != expected {
+		return fmt.Errorf("%s :expected %s, got %s", p.posInfo(),
+			strconv.Quote(string(expected)), strconv.Quote(string(r)))
+	}
+	return nil
+}
+
+func (p *parser) backup() {
+	p.pos -= p.width
+	p.current = p.info[p.pos]
+}
+
+func (p *parser) peek() rune {
+	r := p.next()
+	p.backup()
+	return r
+}
