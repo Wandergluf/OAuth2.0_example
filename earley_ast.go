676e6f008492a1eb13167129fd8fc99ec5a08d69
Wandergluf <104331639+Wandergluf@users.noreply.github.com>
2022-03-27 08:29:22 +0000

    S

--- earley_ast.go
+++ earley_ast.go
@@ -57,3 +57,70 @@ func (p *Parse) buildTrees(state *TableState) []*Node {
 		n := &TableState{state.Term, nil, state.Start, state.End, state.End}
 		cld := []*Node{{n, nil, p}}
 		return cld
+	}
+	if state.Term.Type == List {
+		state.Rb = &RuleBody{}
+		var args []*Arg
+		for i := 0; i < state.Dot; i++ {
+			state.Rb.Terms = append(state.Rb.Terms, &Term{state.Term.Value, Nonterminal, nil})
+			args = append(args, &Arg{"index", i + 1})
+		}
+		state.Rb.F = &FMR{"fmr.list", args}
+	}
+	return p.buildTreesHelper(
+		&[]*Node{}, state, len(state.Rb.Terms)-1, state.End)
+}
+
+/*
+ * How it works: suppose we're trying to match [X -> Y Z W]. We go from finish
+ * to start, e.g., first we'll try to match W in X.encCol. Let this matching
+ * state be M1. Next we'll try to match Z in M1.startCol. Let this matching
+ * state be M2. And finally, we'll try to match Y in M2.startCol, which must
+ * also start at X.startCol. Let this matching state be M3.
+ *
+ * If we matched M1, M2 and M3, then we've found a parsing for X:
+ * X->
+ *    Y -> M3
+ *    Z -> M2
+ *    W -> M1
+ */
+func (p *Parse) buildTreesHelper(children *[]*Node, state *TableState,
+	termIndex, end int) []*Node {
+	// begin with the last --non-terminal-- of the ruleBody of finalState
+	if Debug {
+		fmt.Printf("debug: %+v termIndex:%d children:%+v, end:%d\n",
+			state, termIndex, children, end)
+	}
+	var outputs []*Node
+	var start = -1
+	if termIndex < 0 {
+		// this is the base-case for the recursion (we matched the entire rule)
+		outputs = append(outputs, &Node{state, *children, p})
+		return outputs
+	} else if termIndex == 0 {
+		// if this is the first rule
+		start = state.Start
+	}
+	term := state.Rb.Terms[termIndex]
+
+	if term.Type == Terminal {
+		n := &TableState{term, nil,
+			state.Start + termIndex, state.Start + termIndex + 1, 0}
+		cld := []*Node{{n, nil, p}}
+		cld = append(cld, *children...)
+		for _, node := range p.buildTreesHelper(&cld, state, termIndex-1, end-1) {
+			outputs = append(outputs, node)
+		}
+		return outputs
+	}
+
+	if Debug {
+		fmt.Println("\nend:", end, "term.value:", term.Value, state)
+	}
+	for _, st := range p.columns[end].states {
+		if st == state {
+			// this prevents an endless recursion: since the states are filled in
+			// order of completion, we know that X cannot depend on state Y that
+			// comes after it X in chronological order
+			if Debug {
+				fmt.Println("st==state", st, state)
