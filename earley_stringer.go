38eeed94190dd69237518037ec9d6f143eb702ed
Wandergluf <104331639+Wandergluf@users.noreply.github.com>
2022-04-14 11:37:02 +0000

    some text

--- earley_stringer.go
+++ earley_stringer.go
@@ -73,3 +73,47 @@ func (tc *TableColumn) String() (out string) {
 
 func (p *Parse) String() string {
 	out := ""
+	for _, c := range p.columns {
+		out += c.String() + "\n"
+	}
+	return out
+}
+
+// Print this tree to out
+func (n *Node) Print(out io.Writer) {
+	n.printLevel(out, 0)
+}
+
+func (n *Node) printLevel(out io.Writer, level int) {
+	indentation := ""
+	for i := 0; i < level; i++ {
+		indentation += "  "
+	}
+	fmt.Fprintf(out, "%s%v\n", indentation, n.Value)
+	for _, child := range n.Children {
+		child.printLevel(out, level+1)
+	}
+}
+
+func (n *Node) String() string {
+	if len(n.Children) > 0 {
+		return fmt.Sprintf("%+v %+v", n.Value, n.Children)
+	}
+	return fmt.Sprintf("%+v", n.Value)
+}
+
+func (f *FMR) String() string {
+	if f == nil {
+		return "nf.I($0)"
+	}
+	var args []string
+	invalid := "invalid_fmr"
+	for _, arg := range f.Args {
+		switch arg.Type {
+		case "string":
+			if s, ok := arg.Value.(string); ok {
+				args = append(args, strconv.Quote(s))
+			} else {
+				return invalid
+			}
+		case "int":
