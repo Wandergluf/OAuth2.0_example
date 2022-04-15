8c1414234fbbea56497a8bd6ec85382017b1ebc5
Wandergluf <104331639+Wandergluf@users.noreply.github.com>
2022-04-15 11:08:42 +0000

    JsRender/NodeToJs.vala

--- earley_terminal_match.go
+++ earley_terminal_match.go
@@ -10,4 +10,6 @@ func terminalMatch(term *Term, token *ling.Token) bool {
 	if term == nil || token == nil || term.Type != Terminal {
 		return false
 	}
-	t := gTokens.ge
+	t := gTokens.get(term.Value)
+	if term.Meta == nil || t == nil {
+		if term.Valu
