353e838e476d9708afb2968c76bc540f3953ffed
Wandergluf <104331639+Wandergluf@users.noreply.github.com>
2022-05-15 22:33:06 +0000

    added missing files

--- examples/builtin/main.go
+++ examples/builtin/main.go
@@ -18,4 +18,5 @@ import (
 var (
 	grammar = flag.String("g", "builtin.grammar", "grammar file")
 	js      = flag.String("js", "math.js", "javascript file")
-	input   = flag.String("i", "
+	input   = flag.String("i", "", "file of original text to read")
+	debug   = flag.Bool("
