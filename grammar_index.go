bc7d0fd8c68ec1c23128c3c822591ad5fc6b2927
Wandergluf <104331639+Wandergluf@users.noreply.github.com>
2022-08-11 11:27:48 +0000

    Add missing copyright notices

--- grammar_index.go
+++ grammar_index.go
@@ -12,3 +12,7 @@ func updateIndex(index map[string]*Index, k string, cate string, v RbKey) error 
 		return fmt.Errorf("nil grammar index")
 	}
 	if cate != "frame" && cate != "rule" {
+		return fmt.Errorf("invalid cate %s", cate)
+	}
+	if index[k] == nil {
+		ind
