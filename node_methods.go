5d7b0f512cb36ce37be0ea662cc2f40cda34e4a0
Wandergluf <104331639+Wandergluf@users.noreply.github.com>
2022-12-17 19:00:39 +0000

    added request and cheerio, wrote a hard coded test query to get all of the links on a page

--- node_methods.go
+++ node_methods.go
@@ -13,3 +13,68 @@ import (
 func (n *Node) Pos() *Pos {
 	return n.p.Boundary(n.Value)
 }
+
+// Term returns the root Term of tree node
+func (n *Node) Term() *Term {
+	if n.Value == nil { //|| n.Value.Rb == nil || len(n.Value.Rb.Terms) < 1 {
+		return nil
+	}
+	if n.Value.Term.Value == GammaRule {
+		return n.Value.Rb.Terms[0]
+	}
+	return n.Value.Term
+}
+
+// F returns the FMR signature of node
+func (n *Node) F() *FMR {
+	if n.Value == nil || n.Value.Rb == nil || len(n.Value.Rb.Terms) < 1 {
+		return nil
+	}
+	if n.Value.Term.Value == GammaRule {
+		return n.Children[0].Value.Rb.F
+	}
+	return n.Value.Rb.F
+}
+
+// OriginalText returns the original text of Node n
+func (n *Node) OriginalText() string {
+	pos := n.Pos()
+	return n.p.text[pos.StartByte:pos.EndByte]
+}
+
+// NL returns the normalized text of Node n
+func (n *Node) NL() string {
+	var s []string
+	for i := n.Value.Start + 1; i <= n.Value.End; i++ {
+		s = append(s, n.p.columns[i].token.Text)
+	}
+	return goutil.Join(s)
+}
+
+// Tree returns the parsed tree of Node n
+func (n *Node) Tree() map[string]interface{} {
+	if n.Value.Term.Value == GammaRule {
+		return n.Children[0].Tree()
+	}
+	if n.p == nil {
+		return nil
+	}
+	ret := map[string]interface{}{
+		"type": n.Term().Value,
+		"text": n.OriginalText(),
+		"pos":  n.Pos(),
+		//"fmr":  n.F(),
+	}
+	if n.Value.Rb == nil || n.Value.Rb.F == nil {
+		return ret
+	}
+
+	allTerminal := true
+	for _, node := range n.Children {
+		if node.Value.Term.Type == Terminal {
+			continue
+		}
+		s := node.Value.Term.Value
+		if strings.HasPrefix(s, "g_t_") || strings.HasPrefix(s, "l_t_") {
+			continue
+		}
